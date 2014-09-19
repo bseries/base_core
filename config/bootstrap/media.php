49019c2452cb57d54846fa42106434ed960d88e7
David Persson <davidpersson@gmx.de>
2014-09-19 14:53:25 +0200

    Change/add template search paths, also for mailer.

--- config/bootstrap/media.php
+++ config/bootstrap/media.php
@@ -11,28 +11,52 @@
  */
 
 use lithium\util\Collection;
-use lithium\net\http\Media;
 use lithium\core\Libraries;
+use lithium\net\http\Media as HttpMedia;
+use li3_mailer\net\mail\Media as MailerMedia;
 
 Collection::formats('lithium\net\http\Media');
 
-Media::type('html', 'text/html', [
+//
+// Override media type definitions to set path search order.
+//
+// 1. Always search app first, this allows the app to override everything
+//    independent if a library was given explicitly in the render call or not.
+//
+// 2. Let base_core act as fallback for app searching, same rules apply.
+//
+// 3. Catch cases where library was given.
+//
+HttpMedia::type('html', 'text/html', [
 	'view' => 'lithium\template\View',
 	'paths' => [
 		'template' => [
-			// Replace base_core views with app ones. This is needed for at least the error
-			// templates and might be restricted to just those later.
 			Libraries::get('app', 'path') . '/views/{:controller}/{:template}.{:type}.php',
 			Libraries::get('base_core', 'path') . '/views/{:controller}/{:template}.{:type}.php',
 			'{:library}/views/{:controller}/{:template}.{:type}.php',
 		],
 		'layout'   => [
-			// Force base_core layouts.
-			Libraries::get('base_core', 'path') . '/views/layouts/{:layout}.{:type}.php',
 			Libraries::get('app', 'path') . '/views/layouts/{:layout}.{:type}.php',
-			'{:library}/views/layouts/{:layout}.{:type}.php'
+			Libraries::get('base_core', 'path') . '/views/layouts/{:layout}.{:type}.php',
+			'{:library}/views/layouts/{:layout}.{:type}.php',
 		],
 		// 'element'  => '{:library}/views/elements/{:template}.{:type}.php'
+	]
+]);
+MailerMedia::type('text', 'text/plain', [
+	'view' => 'li3_mailer\template\Mail',
+	'paths' => [
+		'template' => [
+			Libraries::get('app', 'path') . '/mails/:template}.{:type}.php',
+			Libraries::get('base_core', 'path') . '/mails/{:template}.{:type}.php',
+			'{:library}/mails/{:template}.{:type}.php'
+		],
+		'layout'   => [
+			Libraries::get('app', 'path') . '/mails/layouts/{:layout}.{:type}.php',
+			Libraries::get('base_core', 'path') . '/mails/layouts/{:layout}.{:type}.php',
+			'{:library}/mails/layouts/{:layout}.{:type}.php'
+		],
+		// 'element'  => '{:library}/mails/elements/{:template}.{:type}.php'
 	]
 ]);
 
