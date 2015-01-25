e8c30e557020b2fc647512a7d89b434e7a30cfbd
David Persson <davidpersson@gmx.de>
2015-01-25 19:37:02 +0100

    Add index sortable trait.

---
+++ controllers/AdminIndexOrderedTrait.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Base
+ *
+ * Copyright (c) 2013-2014 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+namespace base_core\controllers;
+
+trait AdminIndexSortableTrait {
+
+	public function admin_index() {
+		$model = $this->_model;
+
+		$data = $model::find('all', [
+			'order' => ['order' => 'DESC']
+		]);
+		return compact('data') + $this->_selects();
+	}
+}
+
+?>
