eb7862ca4807c0214eb69ad505072750c3d1babb
David Persson <davidpersson@gmx.de>
2015-01-25 16:04:18 +0100

    Set route formatters and modifiers.

--- config/bootstrap/routes.php
+++ config/bootstrap/routes.php
@@ -11,13 +11,27 @@
  */
 
 use lithium\net\http\Router;
-
+use lithium\util\Inflector;
 $modifiers = [
 	'library' => function($v) {
 		return str_replace('-', '_', $v);
 	},
+	'controller' => function($v) {
+		return Inflector::camelize($v);
+	},
 	'action' => function($v) {
 		return str_replace('-', '_', $v);
+	}
+];
+$formatters = [
+	'library' => function($v) {
+		return str_replace('_', '-', $v);
+	},
+	'controller' => function($v) {
+		return str_replace('_', '-', Inflector::underscore($v));
+	},
+	'action' => function($v) {
+		return str_replace('_', '-', $v);
 	}
 ];
 $persist = ['admin', 'controller'];
@@ -31,27 +45,27 @@ $persist = ['admin', 'controller'];
 Router::connect("/admin/{:library:[a-z\-_]+}/{:controller:[a-z\-]+}", [
 	'action' => 'index',
 	'admin' => true
-], compact('modifiers', 'persist'));
+], compact('modifiers', 'formatters', 'persist'));
 
 // Generic action route.
 // /admin/ecommerce-core/orders/delete/23
 Router::connect("/admin/{:library:[a-z\-_]+}/{:controller:[a-z\-_]+}/{:action:[a-z\-_]+}/{:id:[0-9]+}", [
 	'admin' => true
-], compact('modifiers', 'persist'));
+], compact('modifiers', 'formatters', 'persist'));
 
 // Generic view route.
 // /admin/ecommerce-core/orders/23
 Router::connect("/admin/{:library:[a-z\-_]+}/{:controller:[a-z\-]+}/{:id:[0-9]+}", [
 	'action' => 'view',
 	'admin' => true
-], compact('modifiers', 'persist'));
+], compact('modifiers', 'formatters', 'persist'));
 
 // Generic single action route.
 // /admin/ecommerce-core/orders/add
 // /admin/base-media/media/transfer
 Router::connect("/admin/{:library:[a-z\-_]+}/{:controller:[a-z\-_]+}/{:action:[a-z\-_]+}", [
 	'admin' => true
-], compact('modifiers', 'persist'));
+], compact('modifiers', 'formatters', 'persist'));
 
 // Generic action route with value.
 // /admin/ecommerce-core/orders/update-status/23/checked-out
@@ -59,17 +73,17 @@ Router::connect("/admin/{:library:[a-z\-_]+}/{:controller:[a-z\-_]+}/{:action:[a
 // FIXME: Turn the rules below this commented route into a generic rules.
 // Router::connect("/admin/{:library:[a-z\-_]+}/{:controller:[a-z\-_]+}/{:action:[a-z\-_]+}/{:id:[0-9]+}/{:value}", [
 //	'admin' => true
-// ], compact('modifiers', 'persist'));
+// ], compact('modifiers', 'formatters', 'persist'));
 
 Router::connect("/admin/{:library:[a-z\-_]+}/{:controller:[a-z\-_]+}/update-status/{:id:[0-9]+}/{:status}", [
 	'admin' => true,
 	'action' => 'update_status'
-], compact('modifiers', 'persist'));
+], compact('modifiers', 'formatters', 'persist'));
 
 Router::connect("/admin/{:library:[a-z\-_]+}/{:controller:[a-z\-_]+}/change-role{:id:[0-9]+}/{:role}", [
 	'admin' => true,
 	'action' => 'change_role'
-], compact('modifiers', 'persist'));
+], compact('modifiers', 'formatters', 'persist'));
 
 // Generic API view route.
 // /admin/api/base-core/widgets/total-revenue
@@ -77,13 +91,13 @@ Router::connect('/admin/api/{:library:[a-z\-_]+}/{:controller:[a-z\-_]+}/{:id:([
 	'action' => 'view',
 	'admin' => true,
 	'api' => true
-], compact('modifiers', 'persist'));
+], compact('modifiers', 'formatters', 'persist'));
 
 // Generic API single action/add route.
 // /admin/api/base-media/media/transfer
 Router::connect('/admin/api/{:library:[a-z\-_]+}/{:controller:[a-z\-_]+}/{:action:[a-z\-_]+}', [
 	'admin' => true,
 	'api' => true
-], compact('modifiers', 'persist'));
+], compact('modifiers', 'formatters', 'persist'));
 
 ?>
