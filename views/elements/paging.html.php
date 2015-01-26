2647d9838ab00d501b728bf150a0147935feb05f
David Persson <davidpersson@gmx.de>
2015-01-26 14:57:35 +0100

    Tune styles for tables and pagination.

--- views/elements/paging.html.php
+++ views/elements/paging.html.php
@@ -6,30 +6,30 @@ $pages = $paginator->getPages();
 <?php if ($pages->pageCount > 0): ?>
 	<nav class="nav-paging">
 		<?php if (isset($pages->previous)): ?>
-			<?= $this->html->link('prev', ['action' => 'index', 'page' => $pages->previous], [
-				'rel' => 'prev', 'class' => 'button large'
+			<?= $this->html->link('←', ['action' => 'index', 'page' => $pages->previous], [
+				'rel' => 'prev', 'class' => 'button'
 			]) ?>
 		<?php endif ?>
 		<?php if (!in_array($pages->first, $pages->pagesInRange)): ?>
 			<?= $this->html->link($pages->first, ['action' => 'index', 'page' => $pages->first], [
-				'rel' => 'prev', 'class' => 'button large'
+				'rel' => 'prev', 'class' => 'button'
 			]) ?>
-			…
+			&mdash;
 		<?php endif ?>
 		<?php foreach ($pages->pagesInRange as $page): ?>
 			<?= $this->html->link($page, ['action' => 'index', 'page' => $page], [
-				'class' => 'button large' . ($page === $pages->current ? ' active' : '')
+				'class' => 'button' . ($page === $pages->current ? ' active' : '')
 			]) ?>
 		<?php endforeach ?>
 		<?php if (!in_array($pages->last, $pages->pagesInRange)): ?>
-			…
+			&mdash;
 			<?= $this->html->link($pages->last, ['action' => 'index', 'page' => $pages->last], [
-				'rel' => 'prev', 'class' => 'button large'
+				'rel' => 'prev', 'class' => 'button'
 			]) ?>
 		<?php endif ?>
 		<?php if (isset($pages->next)): ?>
-			<?= $this->html->link('next', ['action' => 'index', 'page' => $pages->next], [
-				'rel' => 'next', 'class' => 'button large'
+			<?= $this->html->link('→', ['action' => 'index', 'page' => $pages->next], [
+				'rel' => 'next', 'class' => 'button'
 			]) ?>
 		<?php endif ?>
 	</nav>
