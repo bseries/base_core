c864284b00432c1ce77f59bc396d0a3d4f46d2cd
David Persson <davidpersson@gmx.de>
2015-04-17 14:57:51 +0200

    Remove error routes.

--- config/routes.php
+++ config/routes.php
@@ -70,30 +70,6 @@ ClientRouter::provide('widgets:view', [
 	'id' => '__ID__'
 ]);
 
-// Error routes for showcasing and developing error pages. Normally those aren't
-// viewed directly. Commonly an exception inside the app will be handled and
-// then the error controller be called.
-//
-// FIXME Move into app.
-Router::connect('/403', [
-	'controller' => 'Errors', 'action' => 'fourohthree', 'library' => 'base_core'
-]);
-Router::connect('/404', [
-	'controller' => 'Errors', 'action' => 'fourohfour', 'library' => 'base_core'
-]);
-Router::connect('/500', [
-	'controller' => 'Errors', 'action' => 'fiveohoh', 'library' => 'base_core'
-]);
-Router::connect('/503', [
-	'controller' => 'Errors', 'action' => 'fiveohthree', 'library' => 'base_core'
-]);
-Router::connect('/maintenance', [
-	'controller' => 'Errors', 'action' => 'maintenance', 'library' => 'base_core'
-]);
-Router::connect('/browser', [
-	'controller' => 'Errors', 'action' => 'browser', 'library' => 'base_core'
-]);
-
 // Generic API view route.
 // /admin/api/base-core/widgets/total-revenue
 Router::connect("/admin/api/{$library}/{$controller}:{:id:([\w\d\-]+|__ID__)}", [
