e6cbd7269ef39cd2ebd5b2b3123ed1dd705fa876
David Persson <davidpersson@gmx.de>
2015-06-10 17:06:07 +0200

    Store user_id on entites on add.

--- controllers/AdminAddTrait.php
+++ controllers/AdminAddTrait.php
@@ -12,6 +12,7 @@
 
 namespace base_core\controllers;
 
+use lithium\security\Auth;
 use lithium\g11n\Message;
 use li3_flash_message\extensions\storage\FlashMessage;
 
@@ -19,11 +20,14 @@ trait AdminAddTrait {
 
 	public function admin_add() {
 		extract(Message::aliases());
+		$user = Auth::check('default');
 
 		$model = $this->_model;
 		$model::pdo()->beginTransaction();
 
-		$item = $model::create();
+		$item = $model::create([
+			'user_id' => $user['id']
+		]);
 
 		$redirectUrl = $this->_redirectUrl($item) + [
 			'action' => 'index', 'library' => $this->_library
