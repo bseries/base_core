5c37bfaef8245cb5cb8b8106d4f28e0b8936187b
David Persson <davidpersson@gmx.de>
2015-06-15 13:49:02 +0200

    Deprecated aggregation trait in favor of behavior.

--- extensions/data/behavior/Aggregated.php
+++ extensions/data/behavior/Aggregated.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Base Core
+ * Base
  *
  * Copyright (c) 2013 Atelier Disko - All rights reserved.
  *
@@ -10,16 +10,16 @@
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  */
 
-namespace base_core\models;
+namespace base_core\extensions\data\behavior;
 
+use lithium\data\Entity;
+use li3_behaviors\data\model\Behavior;
 use lithium\util\Set;
 use lithium\util\Collection;
 use Exception;
 
-trigger_error('AggregationTrait is deprecated in favor of Aggregated behavior.', E_USER_DEPRECATED);
-
 /**
- * This trait allows to combine results from multiple models into one
+ * This behavior allows to combine results from multiple models into one
  * consistent "stream". This is accomplished by overriding the find model
  * method. The behavior of the original find method is tried to be matched
  * as closely as possible. Current finders that are available are `all`,
@@ -28,7 +28,7 @@ trigger_error('AggregationTrait is deprecated in favor of Aggregated behavior.',
  *
  * There are - however - a couple of differences to normal model usage:
  *
- *   - The model this trait is used with *must* be connection-less.
+ *   - The model this behavior is used with *must* be connection-less.
  *   - Models must register to the aggregation stream.
  *   - There is no `order` functionality. As a replacement use the
  *     `sorter` option and provide a custom compare function with it.
@@ -50,15 +50,14 @@ trigger_error('AggregationTrait is deprecated in favor of Aggregated behavior.',
  *
  * @link http://php.net/uasort
  */
-trait AggregationTrait {
+class Aggregated extends \li3_behaviors\data\model\Behavior {
 
-	protected static $_models = [];
+	protected static $_defaults = [
+		// name => fully namespaced
+		'models' => []
+	];
 
-	public static function registerAggregation($model, $name) {
-		static::$_models[$name] = $model;
-	}
-
-	public static function find($type, array $options = []) {
+	public static function aggregate($model, Behavior $behavior, $type, array $options = []) {
 		if (!isset($options['aggregate'])) {
 			throw new Exception('The aggregate option is mandatory.');
 		}
@@ -89,12 +88,12 @@ trait AggregationTrait {
 				// is applied later.
 				$o['limit'] = $options['limit'];
 
-				$model = static::$_models[$n];
+				$_model = $behavior->config('models')[$n];
 
-				foreach ($model::find('all', $o) as $result) {
+				foreach ($_model::find('all', $o) as $result) {
 					// Prefix key with model to make it unique
 					// and allow for quick lookup by index lookup.
-					$data[$model . ':' . $result->id] = static::create([
+					$data[$_model . ':' . $result->id] = $model::create([
 						'original' => $result
 					]);
 				}
@@ -124,10 +123,10 @@ trait AggregationTrait {
 			}
 
 			foreach (Set::normalize($options['aggregate']) as $n => $o) {
-				$model = static::$_models[$n];
+				$_model = $behavior->config('models')[$n];
 
-				if ($result = $model::find('first', (array) $o)) {
-					return static::create(['original' => $result]);
+				if ($result = $_model::find('first', (array) $o)) {
+					return $model::create(['original' => $result]);
 				}
 			}
 			return;
@@ -139,24 +138,18 @@ trait AggregationTrait {
 			$result = 0;
 
 			foreach ($options['aggregate'] as $name => $value) {
-				$model = static::$_models[$name];
-				$result += $model::find('count');
+				$_model = $behavior->config('models')[$name];
+				$result += $_model::find('count');
 			}
 			return $result;
 		} elseif ($type == 'pages') {
-			return floor(static::find('count', $options) / $options['perPage']);
+			return floor($model::find('count', $options) / $options['perPage']);
 		}
 	}
 
-	// Forward method calls to original.
-	public function __call($method, $args) {
-		$entity = array_shift($args);
-		return call_user_func_array([$entity->original, $method], $args);
-	}
-
-	public function type($entity) {
-		foreach (static::$_models as $name => $model) {
-			if ($model === $entity->original->model()) {
+	public function type($model, Behavior $behavior, Entity $entity) {
+		foreach ($behavior->config('models') as $name => $_model) {
+			if ($_model === $entity->original->model()) {
 				return $name;
 			}
 		}
