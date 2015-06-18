512a32d87c7e7dd8f81314466bec4640f7c71c5e
David Persson <davidpersson@gmx.de>
2015-06-18 17:41:51 +0200

    Add access behavior.

---
+++ extensions/data/behavior/Access.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Base
+ *
+ * Copyright (c) 2013 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+namespace base_core\extensions\data\behavior;
+
+use Exception;
+use lithium\data\Entity;
+use li3_behaviors\data\model\Behavior;
+
+class Access extends \li3_behaviors\data\model\Behavior {
+
+	public function hasAccess($model, Behavior $behavior, Entity $entity, $user) {
+		return Access::check('entity', $user, ['request' => $entity], [
+			'rules' => $entity->access
+		]) === [];
+	}
+}
+
+?>
