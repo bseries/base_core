d4f92cd96cadd94009096f08dd314f375544bcb8
David Persson <davidpersson@gmx.de>
2015-07-20 13:22:45 +0200

    Add missing security config file.

---
+++ config/bootstrap/security.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Base Core
+ *
+ * Copyright (c) 2015 Atelier Disko - All rights reserved.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ */
+
+namespace base_core\config\bootstrap;
+
+use lithium\security\validation\FormSignature;
+
+FormSignature::config([
+	'secret' => sha512(PROJECT_SECRET_BASE)
+]);
+
+?>
