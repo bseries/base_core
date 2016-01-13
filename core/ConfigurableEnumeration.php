cf6d2bbd6f51a976a953bc7f942573e2444c496f
David Persson <davidpersson@gmx.de>
2016-01-13 16:16:56 +0100

    Add Configurable enumeration.

---
+++ core/ConfigurableEnumeration.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Base Core
+ *
+ * Copyright (c) 2016 Atelier Disko - All rights reserved.
+ *
+ * Licensed under the AD General Software License v1.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * You should have received a copy of the AD General Software
+ * License. If not, see http://atelierdisko.de/licenses.
+ */
+
+namespace base_core\core;
+
+trait ConfigurableEnumeration {
+
+	public static function enum() {
+		$results = [];
+
+		foreach (static::config(true) as $name => $item) {
+			$results[$name] = $item->title();
+		}
+		return $results;
+	}
+
+	abstract public static function config($name, $config = null);
+}
+
+?>
