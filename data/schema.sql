c0f8e73adfbe4c14aba2199fd4b4821ce298d857
David Persson <davidpersson@gmx.de>
2016-02-24 13:56:12 +0100

    Up schema.

--- data/schema.sql
+++ data/schema.sql
@@ -1,4 +1,3 @@
--- Create syntax for TABLE 'users'
 CREATE TABLE `users` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `uuid` varchar(36) NOT NULL DEFAULT '',
@@ -21,27 +20,6 @@ CREATE TABLE `users` (
   `modified` datetime NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
-  UNIQUE KEY `number` (`number`),
-  KEY `session_key` (`session_key`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Create syntax for TABLE 'virtual_users'
-CREATE TABLE `virtual_users` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `uuid` varchar(36) NOT NULL DEFAULT '',
-  `number` varchar(100) DEFAULT NULL,
-  `session_key` varchar(250) NOT NULL DEFAULT '',
-  `name` varchar(255) DEFAULT '',
-  `email` varchar(100) DEFAULT '',
-  `role` varchar(30) NOT NULL DEFAULT 'user',
-  `locale` varchar(5) DEFAULT 'de',
-  `timezone` varchar(100) NOT NULL DEFAULT 'UTC',
-  `country` char(2) DEFAULT NULL,
-  `is_active` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `is_notified` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  `created` datetime NOT NULL,
-  `modified` datetime NOT NULL,
-  PRIMARY KEY (`id`),
   UNIQUE KEY `number` (`number`),
   KEY `session_key` (`session_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
