27ff1753878788f15b3ad606e66cd45f380efabf
David Persson <davidpersson@gmx.de>
2016-03-29 12:14:31 +0200

    Add support for db host.

--- config/bootstrap/connections.php
+++ config/bootstrap/connections.php
@@ -25,6 +25,8 @@ Connections::add('default', [
 	'encoding' => 'UTF-8',
 	'host' => 'localhost',
 	'persistent' => false,
+	// Using defined for BC, as _HOST was introduced later.
+	'host' => defined('PROJECT_DB_HOST') ? PROJECT_DB_HOST : '127.0.0.1',
 	'database' => PROJECT_DB_DATABASE,
 	'login' => PROJECT_DB_USER,
 	'password' => PROJECT_DB_PASSWORD
