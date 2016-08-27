ca8be243ca857cc0e6ee5992a7e0edaab9e1b7f4
David Persson <davidpersson@gmx.de>
2016-08-27 17:16:22 +0200

    Implement initial users creation in cmd.

--- extensions/command/Users.php
+++ extensions/command/Users.php
@@ -23,15 +23,60 @@ use lithium\core\Libraries;
 
 class Users extends \lithium\console\Command {
 
+	public function initial() {
+		$a = $this->in('Create initial administrator user?', [
+			'choices' => ['y', 'n'],
+			'default' => 'n'
+		]);
+		if ($a === 'y') {
+			$this->_create([
+				'name' => 'Administrator',
+				'email' => 'infra@atelierdisko.de',
+				'is_active' => true,
+				'role' => 'admin',
+				'password' => UsersModel::generatePassword(12, 2)
+			]);
+		}
+		$a = $this->in('Create test users?', [
+			'choices' => ['y', 'n'],
+			'default' => 'n'
+		]);
+		if ($a === 'y') {
+			$this->_create([
+				'name' => 'Kate Miller',
+				'email' => 'kate@atelierdisko.de',
+				'is_active' => true,
+				'role' => 'user',
+				'password' => UsersModel::generatePassword(10, 0)
+			]);
+			$this->_create([
+				'name' => 'John Smith',
+				'email' => 'john@atelierdisko.de',
+				'is_active' => true,
+				'role' => 'client',
+				'password' => UsersModel::generatePassword(10, 0)
+			]);
+		}
+	}
+
 	public function create($name = null, $email = null, $password = null, $role = null) {
 		$data['name'] = $name ?: $this->in('Name');
 		$data['email'] = $email ?: $this->in('Email');
-		$data['password'] = $cleartextPassword = $password ?: $this->in('Password');
+		$data['password'] = $password ?: $this->in('Password', [
+			'default' =>UsersModel::generatePassword(12, 1)
+		]);
 		$data['role'] = $role ?: $this->in('Role');
-
-		$data['password'] = UsersModel::hashPassword($data['password']);
 		$data['is_active'] = true;
+		$this->_create($data);
+	}
 
+	protected function _create(array $data) {
+		$cleartextPassword = null;
+
+		if (isset($data['password'])) {
+			$cleartextPassword = $data['password'];
+			$data['password'] = UsersModel::hashPassword($data['password']);
+		}
 		$user = UsersModel::create($data);
 		$result = $user->save(null, ['validate' => false]);
 
@@ -39,10 +84,10 @@ class Users extends \lithium\console\Command {
 			$this->out('Failed to create user!');
 		} else {
 			$this->out('Created user with:');
-			$this->out('name: '. $data['name']);
-			$this->out('email: '. $data['email']);
-			$this->out('password: '. $cleartextPassword);
-			$this->out('role: '. $data['role']);
+			$this->out('name     : ' . $data['name']);
+			$this->out('email    : ' . $data['email']);
+			$this->out('password : ' . $cleartextPassword);
+			$this->out('role     : ' . $data['role']);
 		}
 	}
 
