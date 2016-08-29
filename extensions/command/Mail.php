50486d00792266b2a08b07bb5dedd6a5383a93fd
David Persson <davidpersson@gmx.de>
2016-08-29 18:46:32 +0200

    Add Mail command for listing available.

---
+++ extensions/command/Mail.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Base Core
+ *
+ * Copyright (c) 2016 Atelier Disko - All rights reserved.
+ *
+ * Licensed under the AD General Software License v1.
+ *
+ * This software is proprietary and confidential. Redistribution
+ * not permitted. Unless required by applicable law or agreed to
+ * in writing, software distributed on an "AS IS" BASIS, WITHOUT-
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * You should have received a copy of the AD General Software
+ * License. If not, see http://atelierdisko.de/licenses.
+ */
+
+namespace base_core\extensions\command;
+
+use lithium\core\Libraries;
+
+class Mail extends \lithium\console\Command {
+
+	/**
+	 * Searches through all base/cms/billing/ecommerce modules
+	 * for available mail templates.
+	 */
+	public function available() {
+		foreach (glob(PROJECT_PATH . '/app/libraries/*/mails/*.php') as $file) {
+			$library = basename(dirname(dirname($file)));
+			$this->out(sprintf('%-30s via %s', basename($file), $library));
+		}
+	}
+}
+
+?>
