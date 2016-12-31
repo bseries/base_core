e95181758f5b6179cea5c82e4795c90edf993064
David Persson <davidpersson@gmx.de>
2016-12-31 11:04:03 +0100

    Add more asset updates.

--- bin/update-assets.sh
+++ bin/update-assets.sh
@@ -15,5 +15,9 @@
 # License. If not, see http://atelierdisko.de/licenses.
 #
 
-curl https://raw.githubusercontent.com/necolas/normalize.css/master/normalize.css \
-	> assets/css/normalize.css
+curl https://raw.githubusercontent.com/necolas/normalize.css/master/normalize.css > assets/css/normalize.css
+curl http://underscorejs.org/underscore.js > assets/js/underscore.js
+curl https://code.jquery.com/jquery-3.1.1.js > assets/js/jquery.js
+curl http://requirejs.org/docs/release/2.3.2/comments/require.js > assets/js/require.js
+curl https://raw.githubusercontent.com/requirejs/domReady/latest/domReady.js > assets/js/require/domready.js
+
