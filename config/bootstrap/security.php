71c21306258923cc5f65b7cc03b67735b208bcd8
David Persson <davidpersson@gmx.de>
2017-05-25 13:04:57 +0200

    Protect against host header attacks.

--- config/bootstrap/security.php
+++ config/bootstrap/security.php
@@ -18,7 +18,10 @@
 namespace base_core\config\bootstrap;
 
 use Exception;
+use lithium\action\Dispatcher;
+use lithium\aop\Filters;
 use lithium\security\validation\FormSignature;
+use base_core\base\Sites;
 
 if (strlen(PROJECT_SECRET_BASE) < 20) {
 	throw new Exception('PROJECT_SECRET_BASE is less than 20 chars.');
@@ -27,5 +30,25 @@ if (strlen(PROJECT_SECRET_BASE) < 20) {
 FormSignature::config([
 	'secret' => hash('sha512', PROJECT_SECRET_BASE)
 ]);
+
+/**
+ * This filter protects against HTTP host header attacks, by matching the `Host` header
+ * sent by the client against a known list of good hostnames. You'll need to modify
+ * the list of hostnames inside the filter before using it.
+ *
+ * @link http://li3.me/docs/book/manual/1.x/quality-code/security
+ * @link http://www.skeletonscribe.net/2013/05/practical-http-host-header-attacks.html
+ */
+Filters::apply(Dispatcher::class, 'run', function($params, $next) {
+	foreach (Sites::registry(true) as $site) {
+		$fqdn = $site->fqdn(true);
+		$host = $params['request']->host;
+
+		if ($host === $fqdn || $host === "www.{$fqdn}") {
+			return $next($params);
+		}
+	}
+	throw new Exception('Suspicious operation detected.');
+});
 
 ?>
