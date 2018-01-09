95345a19ab1da54b840cd9748c6cfdf344addea1
David Persson <nperson@gmx.de>
2018-01-09 17:17:40 +0100

    Add missing FH.

--- models/Base.php
+++ models/Base.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Copyright 2013 David Persson. All rights reserved.
+ * Copyright 2018 Atelier Disko. All rights reserved.
+ *
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
 
 namespace base_core\models;
 
