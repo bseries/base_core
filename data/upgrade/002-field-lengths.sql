27d08f9c5753f90a05a970338ea78dd0d0fe30ec
Marius Wilms <mariuswilms@mailbox.org>
2018-06-17 19:37:22 +0200

    Inherit default charset and collation

--- data/upgrade/002-field-lengths.sql
+++ data/upgrade/002-field-lengths.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `addresses` CHANGE `name` `name` VARCHAR(250)  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT '';
-ALTER TABLE `addresses` CHANGE `company` `company` VARCHAR(250)  CHARACTER SET utf8  COLLATE utf8_general_ci  NULL  DEFAULT NULL;
+ALTER TABLE `addresses` CHANGE `name` `name` VARCHAR(250)  NOT NULL  DEFAULT '';
+ALTER TABLE `addresses` CHANGE `company` `company` VARCHAR(250)  NULL  DEFAULT NULL;
