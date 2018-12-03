8cc72770be2e2aea09a2d2e98e8e41ee2f517267
Marius Wilms <mariuswilms@mailbox.org>
2018-12-03 17:12:29 +0100

    Keep a single connection, even for tests

--- config/bootstrap/connections.php
+++ config/bootstrap/connections.php
@@ -11,25 +11,27 @@ namespace base_core\config\bootstrap;
 
 use lithium\data\Connections;
 
-Connections::add('default', [
+// We define a single "default" connection, under tests, this will point to the test
+// database. This ensures all database request go the the test database, and we cannot
+// forget to point the models' connections to test. For safety reasons do not even define
+// the non-test connection when running tests.
+
+$default = [
 	'type' => 'database',
 	'adapter' => 'MySql',
 	'encoding' => 'utf8mb4',
 	'host' => 'localhost',
 	'persistent' => false,
-	'host' => PROJECT_DB_HOST,
-	'database' => PROJECT_DB_DATABASE,
-	'login' => PROJECT_DB_USER,
-	'password' => PROJECT_DB_PASSWORD
-]);
-
-if (PROJECT_DB_TEST) {
-	Connections::add('test', [
-		'type' => 'database',
-		'adapter' => 'MySql',
-		'encoding' => 'utf8mb4',
-		'host' => 'localhost',
-		'persistent' => false,
+];
+if (!defined('TEST_EXECUTION')) {
+	Connections::add('default', $default + [
+		'host' => PROJECT_DB_HOST,
+		'database' => PROJECT_DB_DATABASE,
+		'login' => PROJECT_DB_USER,
+		'password' => PROJECT_DB_PASSWORD
+	]);
+} elseif (PROJECT_DB_TEST) {
+	Connections::add('default', $default + [
 		'host' => PROJECT_DB_TEST_HOST,
 		'database' => PROJECT_DB_TEST_DATABASE,
 		'login' => PROJECT_DB_TEST_USER,
